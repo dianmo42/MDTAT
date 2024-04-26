ce3d0a3ca1fd27875fb35c7ac359fbadbe2f6340
dianmo42 <dianmo42@outlook.com>
2024-04-26 19:09:06 +0800

    check arguments

--- src/check_args.c
+++ src/check_args.c
@@ -1,7 +1,26 @@
 #include "mdtat.h"
 
-int CheckArgs(int argc, char **argv)
+#include <stdlib.h>
+#include <string.h>
+
+void CheckArgs(int argc, char **argv)
 {
     int iarg;
-    
+
+    for (iarg = 0; iargc < argc; ++iarg)
+    {
+        if (strcmp(argv[iarg], "-in") == 0)
+        {
+            stdin = fopen(argv[iarg + 1], "r");
+            continue;
+        }
+
+        if (strcmp(argv[iarg], "-out") == 0)
+        {
+            stdout = fopen(argv[iarg + 1], "w");
+            continue;
+        }
+    }
+
+    return;
 }
